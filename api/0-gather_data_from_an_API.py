6af825f57a17ad9618c5b56cbd2bc836473f58af
Dtbissy <isaactheedwards@gmail.com>
2024-07-11 13:27:36 -0500

    Number 1 compleeted

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -6,8 +6,22 @@ def get_employee_todos(employee_id):
     
     site = "https://jsonplaceholder.typicode.com/"
     user = requests.get(site + "users/{}".format(employee_id))
-    todos = requests.get(site + "todos?userId={}".format(employee_id))
-    print(site, user, todos)
+    todo = requests.get(site + "todos?userId={}".format(employee_id))
+
+    users = user.json()
+    names = users.get("name")
+    todos = todo.json()
+    # print(site, users, todos, names)
+
+    completed = [i for i in todos if i.get("completed")]
+    all_todos = len(todos)
+    done_todos = len(completed)
+
+    print("Employee {} is done with tasks({}/{}):".format(names, done_todos, all_todos))
+
+    for i in completed:
+        print("\t {}".format(i.get("title")))
+    
 
 
 if __name__ == "__main__":
