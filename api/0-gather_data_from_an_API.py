70d79ae87f467fe1be72595c79b37f6a40ca01ec
Dtbissy <isaactheedwards@gmail.com>
2024-07-11 13:32:01 -0500

    Is that better

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,9 +3,10 @@
 import requests
 from sys import argv
 
+
 def get_employee_todos(employee_id):
     """Stuff"""
-    
+
     site = "https://jsonplaceholder.typicode.com/"
     user = requests.get(site + "users/{}".format(employee_id))
     todo = requests.get(site + "todos?userId={}".format(employee_id))
@@ -19,12 +20,11 @@ def get_employee_todos(employee_id):
     all_todos = len(todos)
     done_todos = len(completed)
 
-    print("Employee {} is done with tasks({}/{}):".format(names, done_todos, all_todos))
+    print("Employee {} is done with tasks({}/{}):"\
+          .format(names, done_todos, all_todos))
 
     for i in completed:
         print("\t {}".format(i.get("title")))
-    
-
 
 if __name__ == "__main__":
     get_employee_todos(employee_id=argv[1])
